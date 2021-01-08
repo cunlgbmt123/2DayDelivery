1321e90e6ad540394925efd0036efa68476bbde0
cunlgbmt123 <tieulong.8b@gmail.com>
2021-01-08 11:04:54 +0700

    adasd

---
+++ app/src/main/java/com/example/a2daydelivery/ChefVerifyPhone.java
@@ -0,0 +1,19 @@
+package com.example.a2daydelivery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChefVerifyPhone extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_verify_phone);
+
+
+    }
+
+}
